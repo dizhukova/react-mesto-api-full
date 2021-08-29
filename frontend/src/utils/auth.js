c45caa8934d516113e2da2d7bbab7c67ca443af3
Diana Zhukova <yndx-dizhukova@yandex.ru>
2021-08-30 00:44:18 +0300

    remove token

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -39,14 +39,14 @@ export const authorize = (email, password) => {
         // });
 };
 
-export const checkToken = (token) => {
+export const checkToken = () => {
     return fetch(`${BASE_URL}/users/me`, {
         method: 'GET',
         credentials: 'include',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
+            // 'Authorization': `Bearer ${token}`,
         }
     })
         .then(checkResponse);
